c207502ec75bf4ae49958a298439c345f8dbf680
brahim <brahimgb1team@gmail.com>
2022-03-11 10:11:18 +0000

    generate api post and get data manipulate dom display data

--- src/index.js
+++ src/index.js
@@ -1,27 +1,2 @@
 import './style.css';
-
-const form = document.getElementById('form');
-const nameInput = document.getElementById('name');
-const scoreInput = document.getElementById('score');
-
-const ul = document.getElementById('ul');
-const list = [];
-
-localStorage.setItem('list', JSON.stringify(list));
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const name = nameInput.value;
-  const score = scoreInput.value;
-
-  const li = document.createElement('li');
-  li.innerHTML = `<li class="name-score"><span>${name}: ${score} <span></li>`;
-  li.classList.add('name-score');
-  ul.appendChild(li);
-  const obj = { Name: name, Score: score };
-  list.push(obj);
-
-  nameInput.value = '';
-  scoreInput.value = '';
-  localStorage.setItem('list', JSON.stringify(list));
-});
+import './api.js';
